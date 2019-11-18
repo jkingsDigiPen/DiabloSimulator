d72ebdeda19950a3fa1df5558ec9bdc6fe10c8bd
jkingsDigiPen <j.kings@digipen.edu>
2019-11-18 09:54:40 -0800

    Move Hero.cs into Game folder.

---
+++ Game/Hero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiabloSimulator.Game
+{
+    public enum HeroClass
+    {
+        Warrior,
+        Rogue,
+        Sorcerer,
+    }
+
+    public class Hero
+    {
+        public Hero(string name_ = "", HeroClass heroClass_ = HeroClass.Warrior)
+        {
+            name = name_;
+            heroClass = heroClass_;
+        }
+
+        public string name;
+        public HeroClass heroClass;
+
+        public static Hero current = new Hero();
+    }
+}

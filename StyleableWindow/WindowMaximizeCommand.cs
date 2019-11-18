b041f1f45425c4538322017f7cac75deaf6f0d1d
jkingsDigiPen <j.kings@digipen.edu>
2019-11-18 08:42:11 -0800

    Add files for styled window behavior.

---
+++ StyleableWindow/WindowMaximizeCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DiabloSimulator.StyleableWindow
+{
+    public class WindowMaximizeCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            var window = parameter as Window;
+
+            if (window != null)
+            {
+                if (window.WindowState == WindowState.Maximized)
+                {
+                    window.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    window.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+    }
+}

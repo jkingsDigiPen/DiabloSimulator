acc3ede78b35e85ab530138c919da31d31c8a698
jkingsDigiPen <j.kings@digipen.edu>
2019-11-18 11:03:05 -0800

    Reorganize file structure of repo.

---
+++ Source/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DiabloSimulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

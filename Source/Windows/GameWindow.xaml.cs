c6b9b30ca6a967c98dbdce5a4368ba0b4790226b
jkingsDigiPen <j.kings@digipen.edu>
2019-11-18 17:48:39 -0800

    Add GameWindow.

---
+++ Source/Windows/GameWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using DiabloSimulator.Game;
+
+namespace DiabloSimulator.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+            btnStart.Click += btnStart_Click;
+            rbClassWarrior.Click += rbClassWarrior_Click;
+            rbClassRogue.Click += rbClassRogue_Click;
+            rbClassSorcerer.Click += rbClassSorcerer_Click;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Hero.current.name = tbxHeroName.Text;
+            MessageBox.Show("Abandon all hope, all ye who enter here.", "Notification");
+        }
+
+        private void rbClassWarrior_Click(object sender, RoutedEventArgs e)
+        {
+            Hero.current.heroClass = HeroClass.Warrior;
+        }
+
+        private void rbClassRogue_Click(object sender, RoutedEventArgs e)
+        {
+            Hero.current.heroClass = HeroClass.Rogue;
+        }
+
+        private void rbClassSorcerer_Click(object sender, RoutedEventArgs e)
+        {
+            Hero.current.heroClass = HeroClass.Sorcerer;
+        }
+    }
+}

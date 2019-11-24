fda08cbfbc3c1a64c39ee06040a91b682b7d123a
jkingsDigiPen <j.kings@digipen.edu>
2019-11-24 15:54:29 -0800

    Move item testing to near top of file, add hat.

--- Source/UserControls/HeroInventoryDisplay.xaml.cs
+++ Source/UserControls/HeroInventoryDisplay.xaml.cs
@@ -45,6 +45,28 @@ namespace DiabloSimulator.UserControls
         // Private Functions:
         //------------------------------------------------------------------------------
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // TO DO: REMOVE THIS
+            Item testItem = new Item("Simple Dagger", SlotType.MainHand, ItemRarity.Common, "Dagger");
+            testItem.stats["MinDamage"] = 2;
+            testItem.stats["MaxDamage"] = 6;
+            testItem.stats["RequiredLevel"] = 1;
+            View.HeroInventory.AddItem(testItem);
+
+            testItem = new Item("Short Sword", SlotType.MainHand, ItemRarity.Common, "1-Handed Sword");
+            testItem.stats["MinDamage"] = 1;
+            testItem.stats["MaxDamage"] = 7;
+            testItem.stats["RequiredLevel"] = 1;
+            View.HeroInventory.AddItem(testItem);
+
+            testItem = new Item("Leather Hood", SlotType.Head, ItemRarity.Magic, "Helm");
+            testItem.stats["Armor"] = 21;
+            testItem.stats["Vitality"] = 4;
+            testItem.stats["RequiredLevel"] = 4;
+            View.HeroInventory.AddItem(testItem);
+        }
+
         private void btnItemEquip_Click(object sender, RoutedEventArgs e)
         {
             int selection = lbInventory.SelectedIndex;
@@ -110,22 +132,6 @@ namespace DiabloSimulator.UserControls
                 return;
             View.HeroInventory.KeepItem(selection);
             lbInventory.SelectedIndex = selection;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            // TO DO: REMOVE THIS
-            Item testItem = new Item("Simple Dagger", SlotType.MainHand, ItemRarity.Common, "Dagger");
-            testItem.stats["MinDamage"] = 2;
-            testItem.stats["MaxDamage"] = 6;
-            testItem.stats["RequiredLevel"] = 1;
-            View.HeroInventory.AddItem(testItem);
-
-            testItem = new Item("Short Sword", SlotType.MainHand, ItemRarity.Common, "1-Handed Sword");
-            testItem.stats["MinDamage"] = 1;
-            testItem.stats["MaxDamage"] = 7;
-            testItem.stats["RequiredLevel"] = 1;
-            View.HeroInventory.AddItem(testItem);
         }
 
         private ViewModel View

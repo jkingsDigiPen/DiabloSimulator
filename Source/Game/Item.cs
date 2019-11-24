4bff5fbd211597016f3ab8e7754dab8b8e73584a
jkingsDigiPen <j.kings@digipen.edu>
2019-11-24 14:11:43 -0800

    Item now has default values for empty items to help avoid breaking bindings to items in the equipment list.

--- Source/Game/Item.cs
+++ Source/Game/Item.cs
@@ -30,7 +30,8 @@ namespace DiabloSimulator.Game
         Hands,
         Waist,
         Feet,
-        Ring,
+        Ring1,
+        Ring2,
         Amulet,
         BothHands,
     }
@@ -48,6 +49,11 @@ namespace DiabloSimulator.Game
         // Public Functions:
         //------------------------------------------------------------------------------
 
+        public Item()
+            : base("NULL")
+        {
+        }
+
         public Item(string name, SlotType slot_, ItemRarity rarity_ = ItemRarity.Common, 
             string baseItem = "") : base(name, baseItem == "" ? name : baseItem)
         {
@@ -56,9 +62,57 @@ namespace DiabloSimulator.Game
             junkStatus = JunkStatus.None;
         }
 
+        public static bool operator==(Item lhs, Item rhs)
+        {
+            // Null items
+            if (lhs is null && !(rhs is null))
+                return false;
+
+            if (!(lhs is null) && rhs is null)
+                return false;
+
+            if (lhs is null && rhs is null)
+                return true;
+
+            // Non-null items
+            if (lhs.slot != rhs.slot)
+                return false;
+
+            if (lhs.archetype != rhs.archetype)
+                return false;
+
+            if (lhs.rarity != rhs.rarity)
+                return false;
+
+            if (lhs.Name != rhs.Name)
+                return false;
+
+            return true;
+        }
+
+        public static bool operator !=(Item lhs, Item rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Item rhs = obj as Item;
+            return this == rhs;
+        }
+
+        public override int GetHashCode()
+        {
+            return (slot.ToString() + archetype + Name + rarity.ToString()).GetHashCode();
+        }
+
         public override string ToString()
         {
-            string itemView = name + " (" + rarity + " " + archetype + ")";
+            // Default/empty item - return empty string
+            if (Name == "NULL")
+                return "";
+
+            string itemView = Name + " (" + rarity + " " + archetype + ")";
             if (junkStatus != JunkStatus.None)
             {
                 itemView += ", marked as " + junkStatus;

4c72f380ac4d037eec2e97d767c3131c1ee42f86
jkingsDigiPen <j.kings@digipen.edu>
2019-11-24 17:58:30 -0800

    Move weapon display into separate control.

---
+++ Source/UserControls/HeroWeaponDisplay.xaml.cs
@@ -0,0 +1,23 @@
+﻿//------------------------------------------------------------------------------
+//
+// File Name:	HeroWeaponDisplay.xaml.cs
+// Author(s):	Jeremy Kings
+// Project:		DiabloSimulator
+//
+//------------------------------------------------------------------------------
+
+using System.Windows.Controls;
+
+namespace DiabloSimulator.UserControls
+{
+    /// <summary>
+    /// Interaction logic for HeroWeaponDisplay.xaml
+    /// </summary>
+    public partial class HeroWeaponDisplay : UserControl
+    {
+        public HeroWeaponDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}

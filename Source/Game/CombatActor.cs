1d2a6f4b0c288e213809596310a5621810f4460d
jkingsDigiPen <j.kings@digipen.edu>
2019-11-24 18:16:44 -0800

    Add interface for combat-capable game objects

---
+++ Source/Game/CombatActor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiabloSimulator.Game
+{
+    interface CombatActor
+    {
+        void Heal(float amount);
+
+        void Damage(float amount);
+
+        float GetAttackDamage();
+    }
+}

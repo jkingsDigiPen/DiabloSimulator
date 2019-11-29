c6004a9eb8d1c71ec9e2252e9d15acd0ed9486de
jkingsDigiPen <j.kings@digipen.edu>
2019-11-29 15:35:35 -0800

    Add Ctrl-S to save, ask to save upon exit.

--- Source/Windows/GameWindow.xaml.cs
+++ Source/Windows/GameWindow.xaml.cs
@@ -7,7 +7,9 @@
 //------------------------------------------------------------------------------
 
 using System;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
 
 namespace DiabloSimulator.Windows
 {
@@ -32,6 +34,8 @@ namespace DiabloSimulator.Windows
 
             // Register events
             ctrlEvents.MonsterChanged += ctrlEvents_MonsterChanged;
+            KeyDown += new KeyEventHandler(Window_KeyDown);
+            Closing += new CancelEventHandler(Window_Closing);
         }
 
         //------------------------------------------------------------------------------
@@ -64,6 +68,52 @@ namespace DiabloSimulator.Windows
             // Don't want our window to be able to get any smaller than this.
             SetValue(MinWidthProperty, this.Width);
             SetValue(MinHeightProperty, this.Height);
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            MessageBoxResult result;
+
+            if(viewModel.InCombat)
+            {
+                result = MessageBox.Show("Warning: Game state cannot be saved during combat. " +
+                    "Unsaved data will be lost upon exit.",
+                    "Diablo Simulator", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            }
+            else
+            {
+                result = MessageBox.Show("Would you like to save your game before quitting?",
+                    "Diablo Simulator", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    viewModel.SaveGame();
+                }
+            }
+
+            if (result == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Ignore commands during combat
+            if (viewModel.InCombat)
+                return;
+
+            // Ctrl S to save
+            if (e.Key == Key.S && Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+            {
+                MessageBoxResult result = MessageBox.Show("Saving your game. This will overwrite your current save for this character.", 
+                    "Diablo Simulator", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+
+                if(result == MessageBoxResult.OK)
+                {
+                    viewModel.SaveGame();
+                }
+            }
         }
 
         private void ctrlEvents_MonsterChanged(object sender, RoutedEventArgs e)
